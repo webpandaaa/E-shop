66118e44b212b019d2e0e8d68c62a81f56a9bd7a
webpandaaa <ankushrajsingh2004@gmail.com>
2024-10-23 22:54:32 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white dark:bg-gray-900 dark:text-white'>
+      <div className='py-4'>
+        <div className="container ">
+            {/* Logo and link section */}
+            <div>
+                <a href="" className='text-2xl text-red-500'>
+                    ESHOP
+                </a>
+            </div>
+            {/* Navbar Right section */}
+            <div>
+
+            </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar

8b79c6da2a081a6020a3042ce9bdcd3895a85be4
webpandaaa <ankushrajsingh2004@gmail.com>
2024-10-24 22:50:52 +0530

    Changes

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='grid grid- gap-4'>
+      <h1>ankush</h1>
+      <h1>aman</h1>
+      <h1>alok</h1>
+    </div>
+  )
+}
+
+export default Hero
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode : "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
-      colors:{
-        primary : "#f42c37",
-        secondary : "#f42c37",
-        brandYellow: "#fdc62e",
-        brandGreen: "#2dcc6f",
-        brandBlue: "#1376f4",
-        brandWhite: "#eeeeee",
+      colors: {
+        primary: "#fea928",
+        secondary: "#ed8900",
       },
-      container:{
-        center : true,
-        padding:{
-          default : "1rem",
-          sm : "3rem",
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
         },
       },
     },
   },
   plugins: [],
-}
+};
